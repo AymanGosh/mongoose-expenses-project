93fdd78d747d63c5a84c034d9be7485ae00bb2ec
aymanab5 <aymansce@gmail.com>
2021-11-30 17:34:54 +0200

    Getting Expenses

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.static(path.join(__dirname, "node_modules")));
 app.use("/", api);
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,4 @@
-onst express = require("express");
+const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Expense = require("../models/Expense");
@@ -6,6 +6,21 @@ const moment = require("moment");
 
 mongoose.connect("mongodb://localhost/expensesDB", { useNewUrlParser: true });
 
+const jsonFile = require("../../expenses.json");
 
+// jsonFile.forEach((jf) =>
+//   new Expense({
+//     name: jf.name,
+//     amount: jf.amount,
+//     date: jf.date,
+//     group: jf.group,
+//   }).save()
+// );
+
+router.get("/expenses", function (req, res) {
+  Expense.find({}, null, { sort: { date: -1 } }, function (err, expenses) {
+    res.send(expenses);
+  });
+});
 
 module.exports = router;
